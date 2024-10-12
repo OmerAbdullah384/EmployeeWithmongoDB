b13d288f416d1939282fc637bd0f29a0c271db58
Omer Abdullah <omerabdullah384@gmail.com>
2024-10-12 18:32:40 +0530

    findById

--- src/main/java/com/mongoproject/mongoexample/controller/MyController.java
+++ src/main/java/com/mongoproject/mongoexample/controller/MyController.java
@@ -1,12 +1,15 @@
 package com.mongoproject.mongoexample.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.mongoproject.mongoexample.models.Employee;
@@ -15,6 +18,8 @@ import com.mongoproject.mongoexample.repository.EmployeeRepo;
 @RestController
 @RequestMapping("/employee")
 public class MyController {
+	
+
 
 	
 	@Autowired
@@ -34,5 +39,12 @@ public class MyController {
 	public List<Employee> getEmployee(){
 		List<Employee> all = employeeRepo.findAll();
 		return all;
+	}
+	
+	@GetMapping("/get/{rollNo}")
+	public Employee getEmployeeById(@PathVariable String rollNo) {
+		Optional<Employee> byId = employeeRepo.findById(rollNo);
+		Employee employee = byId.get();
+		return employee;
 	}
 }
